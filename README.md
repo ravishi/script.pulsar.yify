eeb2590b8326d2f4b68409e845321e43607faa62
Dirley Rodrigues <dirleyrls@gmail.com>
2015-06-19 10:08:35 -0300

    Fix and improve result metadata.
    This is my best take at rip types, rating, resolution and stuff like that.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 script.pulsar.yify
 ==================
 
-YIFY Pulsar provider.
+Pulsar provider for [YIFY](https://yts.to/).
+
+It only returns movies. Only non-3D movies. All returned movies have their *rip type* set as a *BRRip* and are classified as *proper* rips.
--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
 from pulsar import provider
 import itertools
-import string
 
 
 API_ENDPOINT = "https://yts.to/api/v2"
+
+# All results have the same trackers.
+TRACKERS = [
+    'udp://open.demonii.com:1337',
+    'udp://tracker.istole.it:80',
+    'http://tracker.yify-torrents.com/announce',
+    'udp://tracker.publicbt.com:80',
+    'udp://tracker.openbittorrent.com:80',
+    'udp://tracker.coppersurfer.tk:6969',
+    'udp://exodus.desync.com:6969',
+    'http://exodus.desync.com:6969/announce',
+]
+
+# All results have the same rip type. I don't really know if
+# all they are ripped blurays, but since the API doesn't give us
+# that info, and I usually think YIFY rips are always awesome...
+DEFAULT_RIP_TYPE = provider.RIP_BLURAY
 
 
 def pulsarify(movie):
@@ -12,17 +28,21 @@ def pulsarify(movie):
         "name": movie['title_long'],
         "uri": movie['url'],
         "info_hash": t['hash'],
-        #"trackers": string,
+        "trackers": TRACKERS,
         "size": t['size_bytes'],
         "seeds": t['seeds'],
         "peers": t['peers'],
-        "resolution": int(''.join(c for c in t['quality'] if c in string.digits)),
+        "resolution": pulsarify_resolution(t['resolution']),
         #"video_codec": int,
         #"audio_codec": int,
-        #"rip_type": int,
-        "scene_rating": int(movie['rating'] * 100),
+        "rip_type": DEFAULT_RIP_TYPE,
+        "scene_rating": provider.RATING_PROPER,
         #"language": '',
     } for t in movie['torrents'] if not t['quality'].strip().lower() == '3d']
+
+
+def pulsarify_resolution(resolution):
+    return getattr(provider, 'RESOLUTION_{0}'.format(resolution.upper()), provider.RESOLUTION_UNKNOWN)
 
 
 def search(query):
